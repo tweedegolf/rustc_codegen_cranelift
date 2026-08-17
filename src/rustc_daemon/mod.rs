0f0a65b9598dec1c5c973fdbe84a4ae0fd51e15d
David Venhoek <david@tweedegolf.com>
2026-08-17 14:56:15 +0200

    Discard parents jobserver in daemon.
    This ensures the parent's jobserver is not kept around forevere.

--- src/rustc_daemon/mod.rs
+++ src/rustc_daemon/mod.rs
@@ -1,4 +1,5 @@
 use std::io::{Read, Write};
+use std::num::NonZeroUsize;
 use std::os::unix::net::{UnixListener, UnixStream};
 use std::path::PathBuf;
 use std::process::{Command, ExitCode};
@@ -6,6 +7,7 @@ use std::sync::Arc;
 use std::{env, io};
 
 use anstream::AutoStream;
+use rustc_data_structures::jobserver;
 use rustc_driver::{
     Callbacks, DEFAULT_BUG_REPORT_URL, HandledOptions, args, catch_with_exit_code, handle_options,
     init_rustc_env_logger, install_ice_hook, run_compiler,
@@ -115,7 +117,61 @@ struct DaemonCallbacks {
     working_directory: PathBuf,
 }
 
+// Implementation originally from sccache.
+pub unsafe fn discard_inherited_jobserver() {
+    if let Some(value) = ["CARGO_MAKEFLAGS", "MAKEFLAGS", "MFLAGS"]
+        .into_iter()
+        .find_map(|env| std::env::var(env).ok())
+        && let Some(auth) = value.rsplit(' ').find_map(|arg| {
+            arg.strip_prefix("--jobserver-auth=").or_else(|| arg.strip_prefix("--jobserver-fds="))
+        })
+        && !auth.starts_with("fifo:")
+    {
+        let mut parts = auth.splitn(2, ',');
+        let read = parts.next().unwrap();
+        let write = match parts.next() {
+            Some(w) => w,
+            None => return,
+        };
+        let read = read.parse().unwrap();
+        let write = write.parse().unwrap();
+        if read < 0 || write < 0 {
+            return;
+        }
+        unsafe {
+            if libc::fcntl(read, libc::F_GETFD) == -1 {
+                return;
+            }
+            if libc::fcntl(write, libc::F_GETFD) == -1 {
+                return;
+            }
+            libc::close(read);
+            libc::close(write);
+        }
+    }
+}
+
 fn run_daemon() -> ExitCode {
+    // We need to discard the parent's job server otherwise we could keep it around for unrelated compilation sessions.
+    // Safe because nothing is going on yet.
+    unsafe {
+        discard_inherited_jobserver();
+    }
+
+    for key in ["CARGO_MAKEFLAGS", "MAKEFLAGS", "MFLAGS"] {
+        if let Ok(value) = std::env::var(key) {
+            let new_value = value
+                .rsplit(' ')
+                .filter(|flag| !flag.starts_with("--jobserver"))
+                .collect::<Vec<_>>()
+                .join(" ");
+            // Safe because we are running single-threaded at this point.
+            unsafe {
+                env::set_var(key, new_value);
+            }
+        }
+    }
+
     daemonix::Daemonize::new().start().expect("Unable to become a daemon");
 
     let socket_path = socket_path();
@@ -125,6 +181,13 @@ fn run_daemon() -> ExitCode {
     let early_dcx = EarlyDiagCtxt::new(ErrorOutputType::default());
 
     init_rustc_env_logger(&early_dcx);
+    rustc_data_structures::jobserver::initialize(
+        std::thread::available_parallelism().map_or(1, NonZeroUsize::get),
+        |err| {
+            let note = "the build environment is likely misconfigured";
+            early_dcx.early_struct_warn(err).with_note(note).emit()
+        },
+    );
     install_ice_hook(DEFAULT_BUG_REPORT_URL, |_| ());
 
     if let Ok(path) = env::var("__CG_CLIF_DAEMON_NOTIFY") {
@@ -135,6 +198,7 @@ fn run_daemon() -> ExitCode {
     let listener = listener.expect("Unable to open socket");
 
     loop {
+        let token = jobserver::client().acquire().expect("Unable to acquire jobserver token");
         match listener.accept() {
             Ok((mut stream, _)) => {
                 std::thread::spawn(move || {
@@ -150,6 +214,8 @@ fn run_daemon() -> ExitCode {
                             )
                         });
                     }
+                    // Explicit token to keep track that we are running a job.
+                    drop(token);
                 });
             }
             Err(_) => todo!(),
